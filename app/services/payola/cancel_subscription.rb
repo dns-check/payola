6eac84aa8f3a2b6ae76a1d41c31f6b561eda1849
Matt Rideout <mrideout@windserve.com>
2025-11-20 10:33:00 -0500

    Remove usage of the (no longer supported) at_period_end from the cancel endpoint

--- app/services/payola/cancel_subscription.rb
+++ app/services/payola/cancel_subscription.rb
@@ -2,13 +2,20 @@ module Payola
   class CancelSubscription
     def self.call(subscription, options = {})
       secret_key = Payola.secret_key_for_sale(subscription)
-      Stripe::Subscription.cancel(subscription.stripe_id, options, secret_key)
-      
+
       if options[:at_period_end] == true
-        # Store that the subscription will be canceled at the end of the billing period
+        # Schedule cancellation at period end via update
+        # Note: The at_period_end parameter was removed from the cancel endpoint
+        # in Stripe API 2018-08-23. Must use update with cancel_at_period_end instead.
+        Stripe::Subscription.update(
+          subscription.stripe_id,
+          { cancel_at_period_end: true },
+          secret_key
+        )
         subscription.update(cancel_at_period_end: true)
       else
         # Cancel the subscription immediately
+        Stripe::Subscription.cancel(subscription.stripe_id, {}, secret_key)
         subscription.cancel!
       end
     end
