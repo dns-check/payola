3aad7a3079a26fd3daac299cd49b0caec4021b02
Matt Rideout <mrideout@windserve.com>
2025-11-20 13:23:46 -0500

    Improve test coverage of features added or modified during Stripe API upadtes

---
+++ spec/services/payola/card_details_extractor_spec.rb
@@ -0,0 +1,142 @@
+require 'spec_helper'
+
+module Payola
+  describe CardDetailsExtractor do
+    describe ".extract" do
+      context "with nil input" do
+        it "returns nil" do
+          expect(CardDetailsExtractor.extract(nil)).to be_nil
+        end
+      end
+
+      context "with Stripe::Source (card type)" do
+        it "extracts card details from source.card" do
+          source = double('Stripe::Source',
+            is_a?: false,
+            card: double('card',
+              last4: '4242',
+              exp_year: 2025,
+              exp_month: 12,
+              brand: 'Visa'
+            )
+          )
+          allow(source).to receive(:is_a?).with(Stripe::Source).and_return(true)
+          allow(source).to receive(:type).and_return('card')
+
+          result = CardDetailsExtractor.extract(source)
+
+          expect(result[:last4]).to eq '4242'
+          expect(result[:exp_year]).to eq 2025
+          expect(result[:exp_month]).to eq 12
+          expect(result[:brand]).to eq 'Visa'
+        end
+      end
+
+      context "with Stripe::Source (non-card type)" do
+        it "returns nil for non-card source types" do
+          source = double('Stripe::Source')
+          allow(source).to receive(:is_a?).with(Stripe::Source).and_return(true)
+          allow(source).to receive(:is_a?).with(Stripe::Card).and_return(false)
+          allow(source).to receive(:is_a?).with(Stripe::BankAccount).and_return(false)
+          allow(source).to receive(:type).and_return('ach_debit')
+
+          expect(CardDetailsExtractor.extract(source)).to be_nil
+        end
+      end
+
+      context "with Stripe::Card" do
+        it "extracts card details directly" do
+          card = double('Stripe::Card',
+            is_a?: false,
+            last4: '1234',
+            exp_year: 2026,
+            exp_month: 6,
+            brand: 'Mastercard'
+          )
+          allow(card).to receive(:is_a?).with(Stripe::Source).and_return(false)
+          allow(card).to receive(:is_a?).with(Stripe::Card).and_return(true)
+          allow(card).to receive(:respond_to?).with(:brand).and_return(true)
+
+          result = CardDetailsExtractor.extract(card)
+
+          expect(result[:last4]).to eq '1234'
+          expect(result[:exp_year]).to eq 2026
+          expect(result[:exp_month]).to eq 6
+          expect(result[:brand]).to eq 'Mastercard'
+        end
+
+        it "falls back to type when brand is not available" do
+          card = double('Stripe::Card',
+            is_a?: false,
+            last4: '5678',
+            exp_year: 2027,
+            exp_month: 3,
+            type: 'Visa'
+          )
+          allow(card).to receive(:is_a?).with(Stripe::Source).and_return(false)
+          allow(card).to receive(:is_a?).with(Stripe::Card).and_return(true)
+          allow(card).to receive(:respond_to?).with(:brand).and_return(false)
+
+          result = CardDetailsExtractor.extract(card)
+
+          expect(result[:brand]).to eq 'Visa'
+        end
+      end
+
+      context "with Stripe::BankAccount" do
+        it "extracts bank account details with synthetic expiration" do
+          bank_account = double('Stripe::BankAccount',
+            is_a?: false,
+            last4: '6789',
+            bank_name: 'Chase'
+          )
+          allow(bank_account).to receive(:is_a?).with(Stripe::Source).and_return(false)
+          allow(bank_account).to receive(:is_a?).with(Stripe::Card).and_return(false)
+          allow(bank_account).to receive(:is_a?).with(Stripe::BankAccount).and_return(true)
+
+          result = CardDetailsExtractor.extract(bank_account)
+
+          expect(result[:last4]).to eq '6789'
+          expect(result[:brand]).to eq 'Chase'
+          expect(result[:exp_year]).to eq Date.today.year + 1
+          expect(result[:exp_month]).to eq Date.today.month
+        end
+      end
+
+      context "with unknown source type" do
+        it "returns nil" do
+          unknown = double('Unknown')
+          allow(unknown).to receive(:is_a?).with(Stripe::Source).and_return(false)
+          allow(unknown).to receive(:is_a?).with(Stripe::Card).and_return(false)
+          allow(unknown).to receive(:is_a?).with(Stripe::BankAccount).and_return(false)
+
+          expect(CardDetailsExtractor.extract(unknown)).to be_nil
+        end
+      end
+    end
+
+    describe ".expiration_date" do
+      context "with nil input" do
+        it "returns nil" do
+          expect(CardDetailsExtractor.expiration_date(nil)).to be_nil
+        end
+      end
+
+      context "with valid details" do
+        it "creates a date from year and month" do
+          details = { exp_year: 2025, exp_month: 12 }
+          result = CardDetailsExtractor.expiration_date(details)
+
+          expect(result).to eq Date.new(2025, 12, 1)
+        end
+
+        it "handles string values for year and month" do
+          details = { exp_year: '2026', exp_month: '6' }
+          result = CardDetailsExtractor.expiration_date(details)
+
+          expect(result).to eq Date.new(2026, 6, 1)
+        end
+      end
+    end
+  end
+end
--- spec/services/payola/update_card_spec.rb
+++ spec/services/payola/update_card_spec.rb
@@ -24,5 +24,35 @@ module Payola
         expect(@subscription.card_type).to eq 'JCB'
       end
     end
+
+    describe "with Stripe::Source" do
+      before do
+        @plan = create(:subscription_plan)
+
+        token = StripeMock.generate_card_token({})
+        @subscription = create(:subscription, plan: @plan, stripe_token: token, state: 'processing')
+        StartSubscription.call(@subscription)
+        expect(@subscription.error).to be_nil
+        expect(@subscription.active?).to be_truthy
+      end
+
+      it "should extract card details from Stripe::Source" do
+        token2 = StripeMock.generate_card_token({})
+
+        stripe_source = mock_stripe_source(last4: '8888', exp_year: 2030, exp_month: 10, brand: 'Visa')
+
+        customer = double('customer', default_source: 'src_123')
+        allow(customer).to receive_message_chain(:sources, :retrieve).and_return(stripe_source)
+        allow(Stripe::Customer).to receive(:retrieve).and_return(customer)
+        allow(Stripe::Customer).to receive(:update)
+
+        Payola::UpdateCard.call(@subscription, token2)
+
+        @subscription.reload
+        expect(@subscription.card_last4).to eq '8888'
+        expect(@subscription.card_expiration).to eq Date.new(2030, 10, 1)
+        expect(@subscription.card_type).to eq 'Visa'
+      end
+    end
   end
 end
